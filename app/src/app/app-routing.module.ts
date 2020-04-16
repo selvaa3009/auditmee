4ebe1ee1a180bfa989e97063bb9cdb9d22176149
selvaa3009 <selvaa92@gmail.com>
2020-04-16 23:37:34 +0530

    adding basic layouts

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainsectionComponent } from './mainsection/mainsection.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { AnimationsComponent } from './animations/animations.component';
+import { BlankpageComponent } from './blankpage/blankpage.component';
+import { BordersComponent } from './borders/borders.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { CardsComponent } from './cards/cards.component';
+import { ChartsComponent } from './charts/charts.component';
+import { ColorsComponent } from './colors/colors.component';
+import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
+import { OthersComponent } from './others/others.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { TablesComponent } from './tables/tables.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainsectionComponent },
+  { path: 'analytics', component: AnalyticsComponent },
+  { path: 'tabs', component: TabsComponent },
+  { path: 'animation', component: AnimationsComponent },
+  { path: 'blackpage', component: BlankpageComponent },
+  { path: 'borders', component: BordersComponent },
+  { path: 'buttons', component: ButtonsComponent },
+  { path: 'cards', component: CardsComponent },
+  { path: 'charts', component: ChartsComponent },
+  { path: 'colors', component: ColorsComponent },
+  { path: 'forgetpassword', component: ForgetpasswordComponent },
+  { path: 'pagenotfound', component: PagenotfoundComponent },
+  { path: 'tables', component: TablesComponent },
+  { path: 'others', component: OthersComponent },
+  { path: 'tabs', component: TabsComponent },
+  { path: 'blankpage', component: TabsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,6 +7,21 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainsectionComponent } from './mainsection/mainsection.component';
 import { FooterComponent } from './footer/footer.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { CardsComponent } from './cards/cards.component';
+import { ColorsComponent } from './colors/colors.component';
+import { BordersComponent } from './borders/borders.component';
+import { AnimationsComponent } from './animations/animations.component';
+import { OthersComponent } from './others/others.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { BlankpageComponent } from './blankpage/blankpage.component';
+import { ChartsComponent } from './charts/charts.component';
+import { TablesComponent } from './tables/tables.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +29,22 @@ import { FooterComponent } from './footer/footer.component';
     HeaderComponent,
     SidebarComponent,
     MainsectionComponent,
-    FooterComponent
+    FooterComponent,
+    AnalyticsComponent,
+    TabsComponent,
+    ButtonsComponent,
+    CardsComponent,
+    ColorsComponent,
+    BordersComponent,
+    AnimationsComponent,
+    OthersComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgetpasswordComponent,
+    PagenotfoundComponent,
+    BlankpageComponent,
+    ChartsComponent,
+    TablesComponent
   ],
   imports: [
     BrowserModule,
